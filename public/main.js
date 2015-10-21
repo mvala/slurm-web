41a6a8e557ce1b84a9af9ad2055bd489b4abdcfb
Martin Vala <mvala@saske.sk>
2015-10-21 10:57:34 +0300

    Test

--- public/main.js
+++ public/main.js
@@ -2,17 +2,22 @@
  * Main slurm-mon
  */
 
+console.log("We are in main.js");
 var socket = io.connect();
 
-var app = angular.module('slurmMonApp', []) {
+var app = angular.module('slurmMonApp', []);
 
+app.controller("slurmControler", function($scope) {
+
+	$scope.name = "Durak";
 	//Emit ready event.
-	socket.emit('ready') 
+	socket.emit('ready');
 
 	// Listen for the talk event.
 	socket.on('talk', function(data) {
-	    alert(data.message)
+	    console.log(data);
+	    //alert(data.message);
 	}) 
 	
-};
+});
 
